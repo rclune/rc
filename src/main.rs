d5bf9de7fb9402f1e737a804cdf7e5a05c19b755
Sergey Lyskov <sergey.lyskov@jhu.edu>
2025-11-10 21:02:47 -0700

    Add command line argument parsing and minimal tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::{Parser, Subcommand};
+use std::process;
+
+/// A simple command line tool
+#[derive(Parser, Debug)]
+#[command(name = "rc")]
+#[command(version)]
+#[command(about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Verbose mode
+    #[arg(short, long, global = true)]
+    verbose: bool,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Run a command with optional arguments
+    Run {
+        /// The command to run
+        command: String,
+
+        /// Optional arguments for the command
+        #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+        args: Vec<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.verbose {
+        println!("Running in verbose mode");
+        println!("Args: {:#?}", args);
+    }
+
+    match &args.command {
+        Some(Commands::Run {
+            command,
+            args: cmd_args,
+        }) => {
+            println!("Running command: {}", command);
+            if !cmd_args.is_empty() {
+                println!("With arguments: {:?}", cmd_args);
+            }
+            // TODO: Implement actual command execution here
+        }
+        None => {
+            eprintln!("Error: No command specified");
+            eprintln!("Use --help to see available commands");
+            process::exit(1);
+        }
+    }
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,8 +1,17 @@
 use assert_cmd::cargo::cargo_bin_cmd;
 use assert_cmd::prelude::*;
+use predicates::prelude::*;
+
+#[test]
+fn no_command_line_arguments() {
+    cargo_bin_cmd!()
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("Error: No command specified"));
+}
 
 #[test]
 fn help() {
-    let cmd = cargo_bin_cmd!().unwrap();
-    cmd.assert().stdout("Hello, world!\n").success();
+    let cmd = cargo_bin_cmd!().arg("--help").unwrap();
+    cmd.assert().success();
 }
