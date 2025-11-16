ea8233e1d2958daf3e50ccc931830af4b1eb44cc
Sergey Lyskov <sergey.lyskov@jhu.edu>
2025-11-15 19:06:13 -0700

    Add docker tests to GitHub workflow

--- tests/docker.rs
+++ tests/docker.rs
@@ -2,6 +2,8 @@
 
 use assert_cmd::cargo::cargo_bin_cmd;
 use assert_cmd::prelude::*;
+use assert_fs::TempDir;
+use predicates::prelude::*;
 
 mod fixtures;
 
@@ -9,16 +11,44 @@ mod fixtures;
 fn docker_rosetta_score() {
     let fixture = fixtures::ContainerPathShim::new();
     let bin = fixture.install("docker");
-
     let log_file = bin.join("docker.log");
 
+    let root = std::path::PathBuf::from("target/docker");
+    std::fs::create_dir_all(&root).expect("create docker testing dir");
+    let work_dir = TempDir::new_in(root).expect("create temp dir");
+
     let cmd = cargo_bin_cmd!()
-        .args(["run", "rosetta", "score"])
+        .args([
+            "run",
+            "-w",
+            work_dir.path().to_str().unwrap(),
+            "rosetta",
+            "score",
+        ])
         .envs(fixture.env_overrides())
         .env("TEST_INVOCATIONS_LOG", &log_file)
         .unwrap();
     cmd.assert().success();
-    
+
     let log_contents = std::fs::read_to_string(&log_file).expect("Failed to read log file");
 
+    let command_line_parts = [
+        "docker run".into(),
+        format!("--volume {}:/w", work_dir.path().to_str().unwrap()),
+        format!(
+            "--user {}:{}",
+            users::get_current_uid(),
+            users::get_current_gid()
+        ),
+        "--workdir /w".into(),
+    ];
+
+    for command_line_part in &command_line_parts {
+        assert!(
+            predicates::str::contains(command_line_part).eval(&log_contents),
+            "Expected log to contain command line part:\n  {}\n\nActual log contents:\n{}",
+            command_line_part,
+            log_contents
+        );
+    }
 }
