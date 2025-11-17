b2ca2fa00e6cc0c2fc0bdaeeb5736e90c67f1deb
Sergey Lyskov <sergey.lyskov@jhu.edu>
2025-11-16 18:14:58 -0700

    add docker_rosetta_score test

--- tests/docker.rs
+++ tests/docker.rs
@@ -1,14 +1,15 @@
-#![cfg(feature = "test-docker")]
+use std::format;
 
 use assert_cmd::cargo::cargo_bin_cmd;
 use assert_cmd::prelude::*;
 use assert_fs::TempDir;
+
 use predicates::prelude::*;
 
 mod fixtures;
 
 #[test]
-fn docker_rosetta_score() {
+fn mock_docker_rosetta_score() {
     let fixture = fixtures::ContainerPathShim::new();
     let bin = fixture.install("docker");
     let log_file = bin.join("docker.log");
@@ -54,4 +55,53 @@ fn docker_rosetta_score() {
             log_contents
         );
     }
+}
+
+#[test]
+#[cfg(feature = "test-docker")]
+fn docker_rosetta_score() {
+    use assert_fs::assert::PathAssert;
+    use std::fs;
+
+    let root = std::path::PathBuf::from("target/docker");
+    std::fs::create_dir_all(&root).expect("create docker testing dir");
+    let work_dir = TempDir::new_in(root).expect("create temp dir");
+
+    let pdb_id = "1brs";
+    let pdb_file = pdb_id.to_string() + ".pdb";
+    let score_file_name = "output.score";
+
+    let pdb_path = work_dir.path().join(&pdb_file);
+    std::fs::write(
+        pdb_path,
+        reqwest::blocking::get(format!("https://files.rcsb.org/download/{pdb_file}"))
+            .unwrap()
+            .bytes()
+            .unwrap(),
+    )
+    .unwrap();
+
+    let cmd = cargo_bin_cmd!()
+        .args([
+            "run",
+            "-w",
+            work_dir.path().to_str().unwrap(),
+            "rosetta",
+            "score",
+            "-out:file:scorefile",
+            score_file_name,
+            "-in:file:s",
+            &pdb_file,
+        ])
+        .unwrap();
+    cmd.assert().success();
+
+    use assert_fs::prelude::PathChild;
+    let score_file = work_dir.child(score_file_name);
+    score_file.assert(predicates::path::exists());
+
+    let score = fs::read_to_string(&score_file).unwrap();
+
+    assert!(predicates::str::contains("SCORE:").eval(&score));
+    assert!(predicates::str::contains(format!("{pdb_id}_0001")).eval(&score));
 }
